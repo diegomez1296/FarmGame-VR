677a48e7bd463fd8d94b8d503abb51efb915a5bf
Łukasz Rydziński <rlukasz1996@gmail.com>
2020-01-04 06:18:21 +0100

    FarmGame || GameEngine

---
+++ VR_FarmGame/Assets/Assets/Scripts/BuildingsController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingsController : MonoBehaviour {
+
+    [SerializeField] private BuildingType type;
+    [SerializeField] private BuildingBase prototype;
+    [HideInInspector] public BuildingBase[] buildings;
+
+    private float profit;
+    private float price;
+    private float basicPrice;
+
+    private void Start()
+    {
+        switch (type)
+        {
+            case BuildingType.FARM:
+                profit = 1;
+                price = 20;
+                break;
+            case BuildingType.FACTORY:
+                profit = 10;
+                price = 400;
+                break;
+            case BuildingType.BANK:
+                profit = 100;
+                price = 6000;
+                break;
+            default:
+                break;
+        }
+        basicPrice = price;
+    }
+
+    public int GetBuildingsAmount()
+    {
+        buildings = GetComponentsInChildren<BuildingBase>();
+        return buildings.Length;
+    }
+
+    public float GetPrototypeProfit()
+    {
+        return profit;
+    }
+
+    public void CreateBuilding(Vector3 newPosition)
+    {
+        if (GameEngine.FarmValue >= price)
+        {
+            BuildingBase newBuilding = Instantiate(prototype, this.transform);
+            newBuilding.transform.position = newPosition * 10;
+
+            GameEngine.CoordinateX++;
+            if(GameEngine.CoordinateX > 4)
+            {
+                GameEngine.CoordinateX = -4;
+                GameEngine.CoordinateZ++;
+            }
+
+            GameEngine.FarmValue -= price;
+            CurrentPrototypePrice();
+        }
+        Debug.LogError(type + ": " + price);
+    }
+
+    private void CurrentPrototypePrice()
+    {
+        price = GetBuildingsAmount() * GetBuildingsAmount() * profit + basicPrice;
+    }
+}
---
+++ VR_FarmGame/Assets/Assets/Scripts/GameEngine.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEngine : MonoBehaviour {
+
+    public static double FarmValue;
+    public static int CoordinateX;
+    public static int CoordinateZ;
+
+    [SerializeField] private GameObject ground;
+    [HideInInspector] private BuildingsController[] buildingsControllers;
+
+    private int X = -3;
+    private int Y = -4;
+
+
+    private void Start()
+    {
+        FarmValue = 0; //LoadPrefs
+        CoordinateX = -3;
+        CoordinateZ = -4;
+
+        buildingsControllers = ground.GetComponentsInChildren<BuildingsController>();
+
+        InvokeRepeating("GetProfit", 1, 1);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) buildingsControllers[0].CreateBuilding(new Vector3(CoordinateX, -0.1f, CoordinateZ));
+        if (Input.GetKeyDown(KeyCode.Alpha2)) buildingsControllers[1].CreateBuilding(new Vector3(CoordinateX, -0.2f, CoordinateZ));
+        if (Input.GetKeyDown(KeyCode.Alpha3)) buildingsControllers[2].CreateBuilding(new Vector3(CoordinateX, -0.4f, CoordinateZ));
+    }
+
+    private void GetProfit()
+    {
+        double moneyPerSec = 0;
+
+        foreach (var item in buildingsControllers)
+        {
+            //stats
+            moneyPerSec += item.GetBuildingsAmount() * item.GetPrototypeProfit();
+        }
+        FarmValue += moneyPerSec;
+
+        Debug.Log("Farms: <b>" + buildingsControllers[0].GetBuildingsAmount() + "</b>");
+        Debug.Log("Factories: <b>" + buildingsControllers[1].GetBuildingsAmount() + "</b>");
+        Debug.Log("Banks: <b>" + buildingsControllers[2].GetBuildingsAmount() + "</b>");
+        Debug.Log("Money / s: <b>" + moneyPerSec + "</b>");
+        Debug.Log("Farm Value: <b>" + FarmValue +"</b>");
+        Debug.Log("");
+    }
+}
